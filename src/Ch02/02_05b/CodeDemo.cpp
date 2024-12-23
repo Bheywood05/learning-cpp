ab8093133c3aec253c646ab9cb1345d73a70bb6a
Bheywood05 <91253752+Bheywood05@users.noreply.github.com>
2024-12-23 18:20:06 +0000

    More videos

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -6,7 +6,22 @@
 #include <typeinfo>
 
 int main(){
-    
+    auto a = 2;
+    auto b = 123123123123;
+    auto c = 3.14f;
+    auto d = 3.14;
+    auto e = true;
+    auto f = 'd';
+    auto g = "hello world";
+
+    std::cout << "Type of a: " << typeid(a).name() << "!" << std::endl;
+    std::cout << "Type of b: " << typeid(b).name() << "!" << std::endl;
+    std::cout << "Type of c: " << typeid(c).name() << "!" << std::endl;
+    std::cout << "Type of d: " << typeid(d).name() << "!" << std::endl;
+    std::cout << "Type of e: " << typeid(e).name() << "!" << std::endl;
+    std::cout << "Type of f: " << typeid(f).name() << "!" << std::endl;
+    std::cout << "Type of g: " << typeid(g).name() << "!" << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
--- src/Ch02/02_06b/CodeDemo.cpp
+++ src/Ch02/02_06b/CodeDemo.cpp
@@ -3,9 +3,19 @@
 // Preprocessor directives, by Eduardo Corpeño 
 
 #include <iostream>
+#include <string>
+#include <cstdint>
+
+#define CAPACITY 4244
+#define DEBUG
 
 int main(){
-    
+    int32_t a = CAPACITY;
+    uint8_t b = 6;
+#ifdef DEBUG
+    std::cout << "[About to perform addition]" << std::endl;
+#endif
+    std::cout << "the value of a+b is: " << a+b << std::endl;
     std::cout << std::endl << std::endl;
     return (0);
 }
